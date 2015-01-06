006b179b7083cfdcbc9498b231982e6e4fed4dac
Gabriel_Azevedo <gabriel.azevedo95@icloud.com>
2015-01-06 10:07:23 -0800

    Second Commit

--- Multiply/ViewController.m
+++ Multiply/ViewController.m
@@ -2,7 +2,7 @@
 //  ViewController.m
 //  Multiply
 //
-//  Created by Gabriel Borri de Azevedo on 1/5/15.
+//  Created by Gabriel and Tewodros Wondimu on 1/5/15.
 //  Copyright (c) 2015 Mobile Makers. All rights reserved.
 //
 
@@ -23,27 +23,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 }
-- (IBAction)onSliderSlide:(id)sender
+
+- (IBAction)onSliderSlide:(UISlider *)multiplierSlider
 {
-    UISlider *multiplierSlider = (UISlider *)sender;
     int sliderValue = (int)roundf(multiplierSlider.value);
     self.multiplierLabel.text = [NSString stringWithFormat:@"%i",sliderValue];
 }
 
 
-- (IBAction)onCalculateButtonPressed:(id)sender {
-
+- (IBAction)onCalculateButtonPressed:(id)sender
+{
     NSInteger operand1 = [self.numberTextField.text intValue];
     NSInteger operand2 = [self.multiplierLabel.text intValue];
     NSInteger result;
     UIColor *skyColor;
     skyColor = [[UIColor alloc] initWithRed:(102.0/255.0) green:(204.0/255.0) blue:(255.0/255.0) alpha:1.0];
 
-    if (self.operatorSegmentControl.selectedSegmentIndex == 0) {
+    if (self.operatorSegmentControl.selectedSegmentIndex == 0)
+    {
         result = operand1 * operand2;
-    } else {
+    }
+    else
+    {
         result = operand1 / operand2;
     }
+
     if (result > 20)
     {
         self.view.backgroundColor = skyColor;
@@ -52,20 +56,24 @@
     {
         self.view.backgroundColor = [UIColor whiteColor];
     }
+
     if (result % 3 == 0 && result % 5 == 0)
     {
         self.answerLabel.text = @"Fizzbuzz";
     }
-    else if (result % 3 == 0) {
+    else if (result % 3 == 0)
+    {
         self.answerLabel.text = @"Fizz";
     }
-    else if (result % 5 == 0) {
+    else if (result % 5 == 0)
+    {
         self.answerLabel.text = @"Buzz";
     }
     else
     {
         self.answerLabel.text = [NSString stringWithFormat:@"%li", (long)result];
     }
+
     [self.numberTextField resignFirstResponder];
 
 }
